24b0ab86cd7ab8bcd353898654d54637bd8f7239
graemebates <graeme.gilmour.bates@gmail.com>
2022-10-11 22:17:55 -0700

    Refactor to no longer be based on templates, instead all unit tests must be void functions that take a reference to test_restul

--- main.cpp
+++ main.cpp
@@ -5,23 +5,25 @@ int sum(int a, int b){
   return a+b;
 }
 
-void test_negative_inputs(test_result& r){
+const void test_negative_inputs(test_result& r){
   if(sum(1,-2)==-1){
-    r.passed = true;
+    r.passed=true;
   } else {
     r.log="Expected: -1 Got: "+std::to_string(sum(1,-2));
-    r.passed = false;
+    r.passed=false;
   }
 }
 
-void will_fail(test_result& r){
+const void will_fail(test_result& r){
   r.log="Intentional fail";
   r.passed=false;
 }
 
 int main(){
-  Test tester=Test("Sum function tests", "Test Negatives", test_negative_inputs);
+  Test tester=Test("Sum function tests");
+  tester.add_test("Negative Number Test", test_negative_inputs);
   tester.add_test("Will fail test", will_fail);
+  tester.add_test("Negative Number Test", test_negative_inputs);
   tester.run();
   return 0;
 }
--- test.h
+++ test.h
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
-const std::string buff="#### ";
-
-template <typename F>
-struct test_case{
-  F test;
-  std::string description;
-};
+const std::string BUFF="#### ";
+const std::string FAILURE="** ";
 
 struct test_result{
   std::string description;
@@ -16,55 +12,52 @@ struct test_result{
   std::string log;
 };
 
-template <class F>
+struct test_case{
+  std::function<void(test_result&)> test;//
+  std::string description;
+  test_case(){
+    test=NULL;
+    description="";
+  }
+};
+
 class Test{
   public:
-    Test(std::string, std::string, F);
-    void add_test(std::string, F);
+    Test(std::string);
+    void add_test(std::string, const std::function<void(test_result&)>);
     std::vector<test_result> run();
     std::string description;
   private:
-    std::vector<test_case<F>> tests;
+    std::vector<test_case*> tests;
 };
 
-template <class F>
-Test<F>::Test(
-    std::string suite_description,
-    std::string test_description,
-    F test
-  ){
-    test_case<F> blah;
-    description=suite_description;
-    blah.description=test_description;
-    blah.test=test;
-    tests.push_back(blah);
+Test::Test(std::string test_suite_description){
+  description=test_suite_description;
 }
 
-template <class F>
-void Test<F>::add_test(std::string test_description, F test){
-  test_case<F> blah;
-  blah.description=test_description;
-  blah.test=test;
+void Test::add_test(std::string test_description, const std::function<void(test_result&)> test){
+  test_case *blah=new test_case;
+  blah->description=test_description;
+  blah->test=test;
   tests.push_back(blah);
 }
 
-template <class F>
-std::vector<test_result> Test<F>::run(){
+std::vector<test_result> Test::run(){
   int i=1;
   int passed=0;
   int total=tests.size();
   std::vector<test_result> results;
-  std::cout << "Running Test Suite: " << description << std::endl;
+  std::cout << "Running: " << description << std::endl;
   for(auto foo : tests){
-    std::cout << "\t" << buff;
+    std::cout << "\t" << BUFF;
     std::cout << "Running test " << i << " out of " << total << std::endl;
     test_result r;
-    r.description=foo.description;
-    foo.test(r);
+    r.description=foo->description;
+    foo->test(r);
     if(r.passed){
       passed++;
     } else {
-      std::cout << "\t" << buff << r.log << ": " << r.description << std::endl;
+      std::cout << "\t\t" << FAILURE << r.log << ": " << r.description << std::endl;
     }
     i++;
   }
