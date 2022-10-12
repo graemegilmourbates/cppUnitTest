81b2af46f1763e3e2aee8fb2400243dcffbe697a
graemebates <graeme.gilmour.bates@gmail.com>
2022-10-12 12:30:07 -0700

    added a batch test runner

--- main.cpp
+++ main.cpp
@@ -19,11 +19,24 @@ void will_fail(test_result& r){
   r.passed=false;
 }
 
+void test_random_cases(test_result& r){
+  int a = rand();
+  int b = rand();
+  int result=sum(a,b);
+  int expected=a+b;
+  if(result==expected){
+    r.passed=true;
+  } else {
+    r.passed=false;
+  }
+}
+
 int main(){
+  srand((unsigned) time(NULL));
   Test tester=Test("Sum function tests");
   tester.add_test("Negative Number Test", test_negative_inputs);
   tester.add_test("Will fail test", will_fail);
-  tester.add_test("Negative Number Test", test_negative_inputs);
+  tester.add_test("Random inputs", test_random_cases, 10);
   tester.run();
   return 0;
 }
--- test.h
+++ test.h
@@ -13,51 +13,118 @@ struct test_result{
 };
 
 struct test_case{
-  std::function<void(test_result&)> test;//
+  std::function<void(test_result&)> test;
   std::string description;
-  test_case(){
-    test=NULL;
-    description="";
-  }
+};
+
+struct repeating_test{
+  std::function<void(test_result&)> test;
+  std::string description;
+  int repeats;
 };
 
 class Test{
   public:
     Test(std::string);
-    void add_test(std::string, const std::function<void(test_result&)>);
+    ~Test();
+    void add_test(
+      std::string,
+      std::function<void(test_result&)>
+    );
+    void add_test(
+      std::string,
+      std::function<void(test_result&)>,
+      int repeats
+    );
     std::vector<test_result> run();
+    test_result run_repeating_test(repeating_test*);
+
     std::string description;
   private:
     std::vector<test_case*> tests;
+    std::vector<repeating_test*> repeating_tests;
 };
 
 Test::Test(std::string test_suite_description){
   description=test_suite_description;
 }
 
-void Test::add_test(std::string test_description, const std::function<void(test_result&)> test){
-  test_case *blah=new test_case;
-  blah->description=test_description;
-  blah->test=test;
-  tests.push_back(blah);
+Test::~Test(){
+  for(auto test : tests){
+    delete test;
+  }
+}
+
+void Test::add_test(
+    std::string test_description,
+    std::function<void(test_result&)> test
+  ){
+  test_case *tst=new test_case;
+  tst->description=test_description;
+  tst->test=test;
+  tests.push_back(tst);
+}
+
+void Test::add_test(
+  std::string test_description,
+  std::function<void(test_result&)> test,
+  int repeats
+){
+  repeating_test *tst=new repeating_test;
+  tst->description=test_description;
+  tst->test=test;
+  tst->repeats=repeats;
+  repeating_tests.push_back(tst);
+}
+
+test_result Test::run_repeating_test(repeating_test *test){
+  int i=0;
+  int passed=0;
+  test_result batch_result;
+  while(i<test->repeats){
+    test_result r;
+    test->test(r);
+    if(r.passed){ passed++; }
+    i++;
+  }
+  batch_result.description=test->description;
+  batch_result.log="Passed ";
+  batch_result.log+=std::to_string(passed);
+  batch_result.log+=" out of " + std::to_string(test->repeats);
+  if(passed==test->repeats){
+    batch_result.passed=true;
+  } else { batch_result.passed=false; }
+  return batch_result;
 }
 
 std::vector<test_result> Test::run(){
   int i=1;
   int passed=0;
-  int total=tests.size();
+  int total=tests.size()+repeating_tests.size();
   std::vector<test_result> results;
   std::cout << "Running: " << description << std::endl;
-  for(auto foo : tests){
+  for(auto tst : tests){
     std::cout << "\t" << BUFF;
     std::cout << "Running test " << i << " out of " << total << std::endl;
     test_result r;
-    r.description=foo->description;
-    foo->test(r);
+    r.description=tst->description;
+    tst->test(r);
     if(r.passed){
       passed++;
     } else {
-      std::cout << "\t\t" << FAILURE << r.log << ": " << r.description << std::endl;
+      std::cout << "\t\t" << FAILURE;
+      std::cout << r.log << ": " << r.description << std::endl;
+    }
+    i++;
+  }
+  for(auto tst : repeating_tests){
+    std::cout << "\t" << BUFF;
+    std::cout << "Running test " << i << " out of " << total << std::endl;
+    std::cout << "\t\t" << BUFF;
+    std::cout << "Batch test: " << tst->description << std::endl;
+    auto r=run_repeating_test(tst);
+    if(r.passed){
+      passed++;
     }
     i++;
   }
